c0d6075ce9c65d50e925fd74a38706c34d1a03c5
Hyunjun Kim <hyunjun.kim133@gmail.com>
2024-07-16 18:52:09 +0900

    fourth commit

--- hottrack/views.py
+++ hottrack/views.py
@@ -1,11 +1,16 @@
+from io import BytesIO
 import json
+from typing import Literal
 from urllib.request import urlopen
 
+import pandas as pd
+
 from django.db.models import QuerySet, Q
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
+from django.shortcuts import get_object_or_404, render
 
 from hottrack.models import Song
+from hottrack.utils.cover import make_cover_image
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -45,3 +50,44 @@ def index(request: HttpRequest) -> HttpResponse:
             "query": query,
         },
     )
+
+
+def cover_png(request, pk):
+    # 최대값 512, 기본값 256
+    canvas_size = min(512, int(request.GET.get("size", 256)))
+
+    song = get_object_or_404(Song, pk=pk)
+
+    cover_image = make_cover_image(
+        song.cover_url, song.artist_name, canvas_size=canvas_size
+    )
+
+    # param fp : filename (str), pathlib.Path object or file object
+    # image.save("image.png")
+    response = HttpResponse(content_type="image/png")
+    cover_image.save(response, format="png")
+
+    return response
+
+
+def export(request, format: Literal["csv", "xlsx"]):
+    song_qs = Song.objects.all()
+    df = pd.DataFrame(data=song_qs.values())
+
+    export_file = BytesIO()
+
+    if format == "csv":
+        content_type = "text/csv"
+        filename = "hottrack.csv"
+        df.to_csv(path_or_buf=export_file, index=False, encoding="utf-8-sig")
+    elif format == "xlsx":
+        content_type = "application/vnd.ms-excel"
+        filename = "hottrack.xlsx"
+        df.to_excel(excel_writer=export_file, index=False)
+    else:
+        return HttpResponseBadRequest(f"Invalid format : {format}")
+
+    response = HttpResponse(content=export_file.getvalue(), content_type=content_type)
+    response["Content-Disposition"] = 'attachment; filename="{}'.format(filename)
+
+    return response
